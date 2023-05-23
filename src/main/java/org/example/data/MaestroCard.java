e878b3ee9f4b67d96c5e91ac2f6226e3995d050a
qweriikk <dashakots13@gmail.com>
2023-05-23 22:33:56 +0300

    f

---
+++ src/main/java/org/example/data/MaestroCard.java
@@ -0,0 +1,16 @@
+package org.example.data;
+
+import java.io.Serializable;
+import static org.example.data.Country.RU;
+
+public class MaestroCard extends Card implements ICard, Serializable {
+
+    public MaestroCard() {
+        super(PaymentSystem.MAESTRO);
+    }
+
+    @Override
+    protected boolean isCountryValidForTheseCard(Country country) {
+        return RU == country;
+    }
+}
